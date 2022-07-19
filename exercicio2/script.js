2fa48f9e43ed232e7a533d7b13a328f5fa1a8f5f
tcariri <thais.cariri2016@gmail.com>
2022-07-18 22:19:45 -0400

    BarbosaA-Thais-Cariri

---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+const pizza = "você gosta de pizza 4 queijos?"
+
+let pizzaA = prompt ("você gosta de pizza 4 queijos?")
+
+const lasanha = "você gosta de lasanha bolonhesa?"
+
+let lasanhaA = prompt ( "você gosta de lasanha bolonhesa?")
+
+const sushi = " e de sushi você gosta?"
+
+let sushiA = prompt (" e de sushi você gosta?")
+
+console.log(pizza,"-",pizzaA)
+console.log(lasanha,"-",lasanhaA)
+console.log(sushi,"-",sushiA)
+
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+let A = 15
+let B = 30
+let C;
+
+C=A
+A=B
+B=C
+
+console.log("O novo valor de A é",A)
+console.log("O novo valor de B é",B)
+
